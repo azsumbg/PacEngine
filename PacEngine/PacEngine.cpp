a523f4eee80ddb136a5cfd3f2626cf1c3e18ef15
Daniel Dinev <azsumbg@gmail.com>
2024-10-16 19:07:43 +0300

    Add project files.

---
+++ PacEngine/PacEngine.cpp
@@ -0,0 +1,110 @@
+#include "pch.h"
+#include "PacEngine.h"
+
+//RAND ENGINE ***********
+
+gamedll::RANDENGINE::RANDENGINE()
+{
+	for (int i = 0; i < 650; i++) vSeeds.push_back(rd());
+	seed_seq_ptr = new std::seed_seq(vSeeds.begin(), vSeeds.end());
+	twister = new std::mt19937(*seed_seq_ptr);
+}
+gamedll::RANDENGINE::~RANDENGINE()
+{
+	delete seed_seq_ptr;
+	delete twister;
+}
+int gamedll::RANDENGINE::operator()(int min, int max)
+{
+	std::uniform_int_distribution rand_engine{ min, max };
+	return rand_engine(*twister);
+}
+
+//////////////
+
+//ATOM *************
+
+gamedll::ATOM::ATOM(float _x, float _y, float _width = 1.0f, float _height = 1.0f)
+{
+	x = _x;
+	y = _y;
+	width = _width;
+	height = _height;
+}
+float gamedll::ATOM::GetWidth() const
+{
+	return width;
+}
+float gamedll::ATOM::GetHeight() const
+{
+	return height;
+}
+void gamedll::ATOM::SetWidth(float new_width)
+{
+	width = new_width;
+	ex = x + width;
+}
+void gamedll::ATOM::SetHeight(float new_height)
+{
+	height = new_height;
+	ey = y + height;
+}
+void gamedll::ATOM::SetEdges()
+{
+	ex = x + width;
+	ey = y + height;
+}
+void gamedll::ATOM::NewDims(float new_width, float new_height)
+{
+	width = new_width;
+	height = new_height;
+	ex = x + width;
+	ey = y + height;
+}
+
+//////////////
+
+//ATOMPACK *************
+
+void gamedll::ATOMPACK::push_back(ATOM& object)
+{
+	if (next_pushback_pos <= end_pos)
+	{
+		*(mPtr + next_pushback_pos) = object;
+		next_pushback_pos++;
+	}
+}
+void gamedll::ATOMPACK::push_front(ATOM& object)
+{
+	*mPtr = object;
+}
+int gamedll::ATOMPACK::size() const
+{
+	return max_size;
+}
+int gamedll::ATOMPACK::end() const
+{
+	return end_pos;
+}
+gamedll::ATOM& gamedll::ATOMPACK::operator[](int position) const
+{
+	if (position <= end_pos)return *(mPtr + position);
+	return *(mPtr + end_pos);
+}
+
+//CREATURES ***************
+
+bool gamedll::CREATURES::GetFlag(char which_flag) const
+{
+	return(flags && which_flag);
+}
+void gamedll::CREATURES::SetFlag(char which_flag)
+{
+	flags = flags | which_flag;
+}
+void gamedll::CREATURES::ResetFlag(char which_flag)
+{	
+	flags = flags ^ which_flag;
+}
+
+///////////////////////////
---
+++ PacEngine/PacEngine.h
@@ -0,0 +1,128 @@
+#pragma once
+
+#ifdef PACENGINE_EXPORTS 
+#define PAC_API __declspec(dllexport)
+#else
+#define PAC_API __declspec(dllimport)
+#endif
+
+#include <random>
+#include <vector>
+
+enum class dirs { up = 0, down = 1, left = 2, right = 4, stop = 5 };
+enum class creatures { blue = 0, red = 1, pink = 2, orange = 3, hurt = 4, pacman = 5 };
+
+constexpr char up_flag = 0b00000001;
+constexpr char down_flag = 0b00000010;
+constexpr char left_flag = 0b00000100;
+constexpr char right_flag = 0b00001000;
+
+namespace gamedll
+{
+	class PAC_API RANDENGINE
+	{
+		private: 
+			std::random_device rd{};
+			std::vector<unsigned int> vSeeds;
+			std::seed_seq* seed_seq_ptr = nullptr;
+			std::mt19937* twister = nullptr;
+
+		public:
+
+			RANDENGINE();
+			
+			~RANDENGINE();
+
+			int operator()(int min, int max);
+	};
+
+	class PAC_API ATOM
+	{
+		private:
+			float width{};
+			float height{};
+
+		public:
+			float x{};
+			float y{};
+			float ex{};
+			float ey{};
+
+			ATOM(float _x = 1.0f, float _y = 1.0f, float _width = 1.0f, float _height = 1.0f);
+			virtual ~ATOM() {};
+
+			float GetWidth() const;
+			float GetHeight() const;
+
+			void SetWidth(float new_width);
+			void SetHeight(float new_height);
+
+			void SetEdges();
+			void NewDims(float new_width, float new_height);
+	};
+
+	class PAC_API ATOMPACK
+	{
+		private:
+			ATOM* mPtr{ nullptr };
+			int max_size{ 0 };
+			int end_pos{ 0 };
+			int next_pushback_pos{ 0 };
+
+		public:
+
+			ATOMPACK(int lenght) :max_size{ lenght }, mPtr{ new ATOM[lenght]{} } 
+			{
+				end_pos = max_size - 1;
+			};
+			virtual ~ATOMPACK()
+			{
+				if (mPtr)delete[]mPtr;
+			}
+
+			void push_back(ATOM& object);
+			void push_front(ATOM& object);
+			
+			int size() const;
+			int end() const;
+
+			ATOM& operator[](int position) const;
+
+	};
+
+	class PAC_API CREATURES :public ATOM
+	{
+		protected:
+			creatures type = creatures::pacman;
+
+			int max_frames{};
+			int current_frame{};
+			int frame_delay = 5;
+
+			int hurt_delay = 1000;
+
+			float speed = 1.0f;
+
+			CREATURES(creatures what_type, float _x, float _y) :ATOM(_x, _y) {};
+
+			char flags{ 0 };
+
+		public:
+			bool panic = false;
+			ATOMPACK* ObstaclesPack{};
+			
+			virtual ~CREATURES() {};
+			virtual void Release() = 0;
+
+			virtual void Move(float gear) = 0;
+			virtual void Hurt() = 0;
+			void SetFlag(char which_flag);
+			void ResetFlag(char which_flag);
+			bool GetFlag(char which_flag) const;
+	};
+
+	typedef CREATURES* Creature;
+
+	extern PAC_API Creature Factory(creatures what, float startX, float startY);
+}
+
